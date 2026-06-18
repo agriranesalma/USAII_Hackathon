8ddcde1a62c0848ff9040b43c8bf0b7dcfaa3cb0
Salma Agrirane <agriranesalma99@gmail.com>
2026-06-18 17:03:43 +0100

    Fix formatting of title in app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.set_page_config(page_title="First Gen Compass", page_icon="🗺️", layout="centered")
-st.title("First-gen Compass", layout="centered")
+st.title("First-gen Compass")
